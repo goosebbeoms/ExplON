fbdbcd226d3a7611c0e08e9a46fc2606a3ad2c30
goosebbeoms <seo980916@gmail.com>
2024-12-17 15:43:27 +0900

    chore: backend 작업 환경 초기 세팅

---
+++ backend/src/main/java/com/explon/explon/ExplonApplication.java
@@ -0,0 +1,13 @@
+package com.explon.explon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplonApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/explon/explon/ExplonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explon.explon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
